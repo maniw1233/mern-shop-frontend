d09a90fee89767f716d3a5ac2280fee47dcd3d3c
Manikanta Kharvi <126985671+maniw1233@users.noreply.github.com>
2025-07-02 09:51:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,73 +8,89 @@ import { Logout } from './features/auth/components/Logout';
 import { Protected } from './features/auth/components/Protected';
 import { useAuthCheck } from "./hooks/useAuth/useAuthCheck";
 import { useFetchLoggedInUserDetails } from "./hooks/useAuth/useFetchLoggedInUserDetails";
-import { AddProductPage, AdminOrdersPage, CartPage, CheckoutPage, ForgotPasswordPage, HomePage, LoginPage, OrderSuccessPage, OtpVerificationPage, ProductDetailsPage, ProductUpdatePage, ResetPasswordPage, SignupPage, UserOrdersPage, UserProfilePage, WishlistPage } from './pages';
+import {
+  AddProductPage,
+  AdminOrdersPage,
+  CartPage,
+  CheckoutPage,
+  ForgotPasswordPage,
+  HomePage,
+  LoginPage,
+  OrderSuccessPage,
+  OtpVerificationPage,
+  ProductDetailsPage,
+  ProductUpdatePage,
+  ResetPasswordPage,
+  SignupPage,
+  UserOrdersPage,
+  UserProfilePage,
+  WishlistPage
+} from './pages';
 import { AdminDashboardPage } from './pages/AdminDashboardPage';
 import { NotFoundPage } from './pages/NotFoundPage';
 
-
 function App() {
 
-  const isAuthChecked=useSelector(selectIsAuthChecked)
-  const loggedInUser=useSelector(selectLoggedInUser)
-
+  const isAuthChecked = useSelector(selectIsAuthChecked);
+  const loggedInUser = useSelector(selectLoggedInUser);
 
   useAuthCheck();
   useFetchLoggedInUserDetails(loggedInUser);
 
-
   const routes = createBrowserRouter(
     createRoutesFromElements(
       <>
-        <Route path='/signup' element={<SignupPage/>}/>
-        <Route path='/login' element={<LoginPage/>}/>
+        {/* Public Routes */}
+        <Route path='/' element={<LoginPage />} />
+        <Route path='/signup' element={<SignupPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/forgot-password' element={<ForgotPasswordPage />} />
+        <Route path='/reset-password/:userId/:passwordResetToken' element={<ResetPasswordPage />} />
+
+        {/* OTP only if not verified */}
         <Route
-  path="/verify-otp"
-  element={
-    loggedInUser ?<Navigate to="/login" /> : <OtpVerificationPage />
-  }
-/>
- 
-        <Route path='/forgot-password' element={<ForgotPasswordPage/>}/>
-        <Route path='/reset-password/:userId/:passwordResetToken' element={<ResetPasswordPage/>}/>
-        <Route exact path='/logout' element={<Protected><Logout/></Protected>}/>
-        <Route exact path='/product-details/:id' element={<Protected><ProductDetailsPage/></Protected>}/>
+          path="/verify-otp"
+          element={
+            loggedInUser && !loggedInUser.isVerified ? (
+              <OtpVerificationPage />
+            ) : (
+              <Navigate to="/" />
+            )
+          }
+        />
 
-        {
-          loggedInUser?.isAdmin?(
-            // admin routes
-            <>
-            <Route path='/admin/dashboard' element={<Protected><AdminDashboardPage/></Protected>}/>
-            <Route path='/admin/product-update/:id' element={<Protected><ProductUpdatePage/></Protected>}/>
-            <Route path='/admin/add-product' element={<Protected><AddProductPage/></Protected>}/>
-            <Route path='/admin/orders'  element={<Protected><AdminOrdersPage/></Protected>}/>
-            <Route path='*' element={<Navigate to={'/admin/dashboard'}/>}/>
-            </>
-          ):(
-            // user routes
-            <>
-           <Route path='/' element={<Protected><HomePage/></Protected>}/>
+        {/* Protected Routes */}
+        <Route path='/logout' element={<Protected><Logout /></Protected>} />
+        <Route path='/product-details/:id' element={<Protected><ProductDetailsPage /></Protected>} />
 
-            <Route path='/cart' element={<Protected><CartPage/></Protected>}/>
-            <Route path='/profile' element={<Protected><UserProfilePage/></Protected>}/>
-            <Route path='/checkout' element={<Protected><CheckoutPage/></Protected>}/>
-            <Route path='/order-success/:id' element={<Protected><OrderSuccessPage/></Protected>}/>
-            <Route path='/orders' element={<Protected><UserOrdersPage/></Protected>}/>
-            <Route path='/wishlist' element={<Protected><WishlistPage/></Protected>}/>
-            </>
-          )
-        }
+        {loggedInUser?.isAdmin ? (
+          // Admin Routes
+          <>
+            <Route path='/admin/dashboard' element={<Protected><AdminDashboardPage /></Protected>} />
+            <Route path='/admin/product-update/:id' element={<Protected><ProductUpdatePage /></Protected>} />
+            <Route path='/admin/add-product' element={<Protected><AddProductPage /></Protected>} />
+            <Route path='/admin/orders' element={<Protected><AdminOrdersPage /></Protected>} />
+            <Route path='*' element={<Navigate to='/admin/dashboard' />} />
+          </>
+        ) : (
+          // User Routes
+          <>
+            <Route path='/home' element={<Protected><HomePage /></Protected>} />
+            <Route path='/cart' element={<Protected><CartPage /></Protected>} />
+            <Route path='/profile' element={<Protected><UserProfilePage /></Protected>} />
+            <Route path='/checkout' element={<Protected><CheckoutPage /></Protected>} />
+            <Route path='/order-success/:id' element={<Protected><OrderSuccessPage /></Protected>} />
+            <Route path='/orders' element={<Protected><UserOrdersPage /></Protected>} />
+            <Route path='/wishlist' element={<Protected><WishlistPage /></Protected>} />
+          </>
+        )}
 
-        <Route path='*' element={<NotFoundPage/>} />
-
+        <Route path='*' element={<NotFoundPage />} />
       </>
     )
-  )
+  );
 
-  
-  return isAuthChecked ? <RouterProvider router={routes}/> : "";
+  return isAuthChecked ? <RouterProvider router={routes} /> : null;
 }
 
-export default App;   
-
-i am getting problem in project that is , i need login page to be displayed first not otp page in versel
+export default App;
